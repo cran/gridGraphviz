5793ee2d02c221be15b61ab2c7af3805e011e6a8
Paul Murrell <p.murrell@auckland.ac.nz>
2015-06-15 00:00:00 +0000

    version 0.3

--- R/graph.R
+++ R/graph.R
@@ -1,16 +1,16 @@
 
-# Draw an Ragraph laid out graph using grid
+### Draw an Ragraph laid out graph using grid
 
-# Code for "node" grobs
+## Code for "node" grobs
 
 node <- function(label, x=.5, y=.5,
                  shape="plain",
                  height=NULL, lwidth=NULL, rwidth=NULL,
-                 name=label,
+                 name,
                  color="black",
                  fillcolor="transparent",
                  fontcolor="black",
-                 fontsize=10) {
+                 fontsize=10) {    
     # Shrink text if necessary
     cex <- 1
     if (!(is.null(height) || is.null(lwidth) || is.null(rwidth))) {
@@ -28,9 +28,10 @@ node <- function(label, x=.5, y=.5,
         if (th > nh && nh/th < cex)
             cex <- nh/th
     }
-    lab <- textGrob(label, x, y,
-                    gp=gpar(col=fontcolor, fontsize=fontsize, cex=cex),
-                    name="label")
+    
+    # make label grob
+    lab <- makeLabelGrob(label, x, y, fontcolor, fontsize, cex, name=name)
+    
     if (is.null(height)) {
         height <- grobHeight(lab)
     }
@@ -38,22 +39,12 @@ node <- function(label, x=.5, y=.5,
         lwidth <- 0.5*grobWidth(lab)
         rwidth <- 0.5*grobWidth(lab)
     }
-    if (shape == "circle") {
-        box <- circleGrob(x, y, r=lwidth, name="box",
-                         gp=gpar(col=color, fill=fillcolor))
-    } else if (shape == "box") {
-        box <- rectGrob(x, y,
-                        width=lwidth + rwidth,
-                        height=height,
-                        name="box",         
-                        gp=gpar(col=color, fill=fillcolor))
-    } else { # plain
-        warning("Unsupported node shape; using 'box'")
-        box <- rectGrob(x, y,
-                        width=lwidth + rwidth,
-                        height=height,
-                        name="box", gp=gpar(col=NA, fill=NA))
-    }
+
+    # make box grob
+    box <- makeBoxGrob(shape=shape, name=name, x=x, y=y, height=height,
+                       lwidth=lwidth, rwidth=rwidth, color=color,
+                       fillcolor=fillcolor)
+    
     gTree(children=gList(box, lab),
           name=gsub("\n", "", name), cl="node")
 }
@@ -77,11 +68,11 @@ drawNode <- function(node) {
     lwidth <- getNodeLW(node)
     rwidth <- getNodeRW(node)
     col <- color(node)
-    if (is.na(col)) {
+    if (is.na(col) || col == "") {
         col <- "black"
     }
     fill <- fillcolor(node)
-    if (is.na(fill)) {
+    if (is.na(fill) || fill == "") {
         fill <- "transparent"
     }
     fontcol <- labelColor(txtLabel(node))
@@ -91,21 +82,23 @@ drawNode <- function(node) {
     fontsize <- labelFontsize(txtLabel(node))
     label <- labelText(txtLabel(node))
     xy <- getNodeXY(node)
-    grid.draw(node(label,
-                   unit(xy$x, "native"),
-                   unit(xy$y, "native"),
+    name <- name(node)
+    grid.draw(node(label=label,
+                   x=unit(xy$x, "native"),
+                   y=unit(xy$y, "native"),
                    shape=shape,
                    height=unit(height, "native"),
                    lwidth=unit(lwidth, "native"),
                    rwidth=unit(rwidth, "native"),
-                   color=col, fillcolor=fill,
+                   name=name, color=col, fillcolor=fill,
                    fontcolor=fontcol, fontsize=fontsize))
 }
 
-curveGrob <- function(curve, col, lwd, lty) {
+makeCurve <- function(curve, col, lwd, lty, name) {
     controlPoints <- pointList(curve)
     bezierGrob(unit(sapply(controlPoints, "[" ,1), "native"),
                unit(sapply(controlPoints, "[" ,2), "native"),
+	       name=name,
                gp=gpar(col=col, lwd=lwd, lty=lty))
 }
 
@@ -113,31 +106,106 @@ drawCurve <- function(curve, col, lwd, lty) {
     grid.draw(curveGrob(curve, col, lwd, lty))
 }
 
-drawDetails.edgegrob <- function(x, ...) {
-    edge <- x$edge
+makeEdge <- function(edge, edgemode) {
+    name <- paste(tail(edge), head(edge), sep="~")
     if (!length(edge@lwd))
         edge@lwd <- 1    
     if (!length(edge@lty))
         edge@lty <- "solid"
-    # FIXME:  assumes arrow at end of edge
     splines <- splines(edge)
     n <- length(splines)
+    curveNames <- paste("curve", name, seq(along=splines), sep="-")
     col <- color(edge)
-    lapply(splines, drawCurve, col=col, lwd=edge@lwd, lty=edge@lty)
+    curves <- mapply(makeCurve, splines, col=col, lwd=edge@lwd, lty=edge@lty,
+		 name=curveNames, SIMPLIFY=FALSE)
+    
+    # Edge label
+    if (length(labelText(txtLabel(edge))) != 0) {
+        fontcol <- labelColor(txtLabel(edge))
+        if (length(fontcol) == 0) {
+            fontcol <- "black"
+        }
+        fontsize <- labelFontsize(txtLabel(edge))
+        label <- labelText(txtLabel(edge))
+        xy <- labelLoc(txtLabel(edge))
+        x <- unit(getX(xy), "native")
+        y <- unit(getY(xy), "native")
+        cex <- 1
+        lab <- list(makeLabelGrob(label, x, y, fontcol, fontsize, cex, name))
+    } else {
+        lab <- list()
+    }
+        
+    firstCP <- pointList(splines[[n]])[[1]]
     lastCP <- pointList(splines[[n]])[[4]]
-    arrow <- arrow(angle=10, type="closed", length=x$arrowlen)
-    grid.segments(lastCP[1], lastCP[2],
-                  getX(ep(edge)), getY(ep(edge)),
-                  default.units="native",
-                  arrow=arrow,
-                  gp=gpar(col=col, fill=col, lwd=edge@lwd, lty=edge@lty))
+    arrowsize <- as.numeric(arrowsize(edge))
+    arrowhead <- arrowhead(edge)
+    arrowtail <- arrowtail(edge)
+
+    ## APOLOGY and EXPLANATION
+    ##
+    ## The "back" and "forward" arrows go through some contorted conditionals
+    ## before making arrow ojects. This is mostly because of the way edge@sp and
+    ## edge@ep are produced. A directed graph will have a sensible edge@ep for
+    ## every edge with an arrow at the end (or edge@sp for an arrow at the
+    ## start). This is used to plot the arrow between the last control point
+    ## of an edge's splines and the border of the node.
+    ##
+    ## An undirected graph does not require edeg@sp or edge@ep values.
+    ##
+    ## Unfortunately Rgraphviz v.2.6.0 will produced meaningless edge@sp and
+    ## edge@ep values on undirected graphs - values of "0,0", "NA,0" or "0,NA".
+    ## For some as-yet-undiscovered reason it will also occasionally
+    ## produce these types of values on a directed graph where no arrow is
+    ## required.
+    ##
+    ## As a result, before attempting to draw an arrow the code will
+    ## check for the following things:
+    ##   1) is the edgemode of this graph undirected?
+    ##   2) is the direction of this edge such that no arrow should be drawn
+    ##      here? e.g. "back" arrow on "forward" edge
+    ##   3) is the X-value in the sp/ep NA?
+    ##   4) is the Y-value in the sp/ep NA?
+    ##   5) is sp/ep equal to "0,0"?
+    ## and if any of these is true an arrow is NOT drawn.
+    ##
+    ## Only if none of these conditions is true will an arrow be drawn.
+    
+    # "back" arrow    
+    if (edgemode == "undirected" || edge@dir == "forward" ||
+        (is.na(getX(sp(edge))) || is.na(getY(sp(edge)))) ||
+        (getX(sp(edge)) == 0 && getY(sp(edge)) == 0)) {
+      start <- list()
+    } else {
+      start <- list(makeArrowGrob(arrowtail, arrowsize,
+                                  firstCP[1], firstCP[2], 
+                                  getX(sp(edge)), getY(sp(edge)), 
+                                  col, edge@lwd, edge@lty,
+                                  name=paste("arrowtail", name, sep="-")))
+    }
+    
+    # "forward" arrow
+    if (edgemode == "undirected" || edge@dir == "back" ||
+        (is.na(getX(ep(edge))) || is.na(getY(ep(edge)))) ||
+        (getX(ep(edge)) == 0 && getY(ep(edge)) == 0)) {
+      end <- list()
+    } else {
+      end <- list(makeArrowGrob(arrowhead, arrowsize,
+                                lastCP[1], lastCP[2], 
+                                getX(ep(edge)), getY(ep(edge)), 
+                                col, edge@lwd, edge@lty,
+                                name=paste("arrowhead", name, sep="-")))
+    }
+    
+    gTree(children=do.call("gList", c(curves, start, end, lab)),
+          name=paste("edge", name, sep="-"))
 }
 
-drawEdge <- function(edge, arrowlen) {
-    grid.draw(grob(edge=edge, arrowlen=arrowlen, cl="edgegrob"))
+drawEdge <- function(edge, edgemode) {
+    grid.draw(makeEdge(edge, edgemode))
 }
 
-grid.graph <- function(rag, newpage=FALSE) {
+grid.graph <- function(rag, newpage=FALSE, nodesOnTop=TRUE) {
     if (!is(rag, "Ragraph") || !laidout(rag))
         stop("Must have a laid out Ragraph object")
     if (newpage) {
@@ -147,19 +215,24 @@ grid.graph <- function(rag, newpage=FALSE) {
     # The order is the same as the nodes in
     # the original graphNEL
     bb <- boundBox(rag)
-    # FIXME: needs better calculation for arrow head size!
-    # Set arrowlen based on graph size
-    arrowlen <- min(.02*unit(getX(upRight(bb)) - getX(botLeft(bb)), "native"),
-                    .02*unit(getY(upRight(bb)) - getY(botLeft(bb)), "native"))
     # Ensure aspect ratio
-    pushViewport(viewport(layout=grid.layout(1, 1,
-                            width=(getX(upRight(bb)) - getX(botLeft(bb))) /
-                                  (getY(upRight(bb)) - getY(botLeft(bb))),
-                            respect=TRUE)))
+    pushViewport(viewport(width=unit(getX(upRight(bb)), "points"),
+                          height=unit(getY(upRight(bb)), "points"),
+                          layout=grid.layout(1, 1,
+                              widths=(getX(upRight(bb)) - getX(botLeft(bb))) /
+                                     (getY(upRight(bb)) - getY(botLeft(bb))),
+                              respect=TRUE),
+                          name="graphSizeVP"))
     pushViewport(viewport(layout.pos.col=1,
                           xscale=c(getX(botLeft(bb)), getX(upRight(bb))),
-                          yscale=c(getY(botLeft(bb)), getY(upRight(bb)))))
-    lapply(AgEdge(rag), drawEdge, arrowlen)
-    lapply(AgNode(rag), drawNode)
+                          yscale=c(getY(botLeft(bb)), getY(upRight(bb))),
+                          name="graphScaleVP"))
+    if (nodesOnTop) {
+        lapply(AgEdge(rag), drawEdge, edgemode(rag))
+        lapply(AgNode(rag), drawNode)
+    } else {
+        lapply(AgNode(rag), drawNode)
+        lapply(AgEdge(rag), drawEdge, edgemode(rag))
+    }
     upViewport(2)
 }
